ca5efc8bfd7c056087bfd11b071993b54b4200e1
yamiew511 <yamiew511@gmail.com>
2022-11-08 15:53:56 +0800

    v2

---
+++ NugetTrial_Jerry1108/NugetTrialClass_1108.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetTrial_Jerry1108
+{
+    internal class NugetTrialClass_1108
+    {
+    }
+}
--- NugetTrial_Jerry1108/obj/Debug/net6.0/NugetTrial_Jerry1108.AssemblyInfo.cs
+++ NugetTrial_Jerry1108/obj/Debug/net6.0/NugetTrial_Jerry1108.AssemblyInfo.cs
@@ -11,11 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NugetTrial_Jerry1108")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("_CompanyName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("some description of this package")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("NugetTrial_Jerry1108")]
+[assembly: System.Reflection.AssemblyProductAttribute("NugetTrial_Jerry1108_ProductName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NugetTrial_Jerry1108")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
